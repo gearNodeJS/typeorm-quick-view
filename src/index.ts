df707a1249796e19c4e51e9ed2297795a46dd63a
Valery Semenenko <gearmobile@gmail.com>
2021-01-04 18:24:44 +0300

    find, alter and remove from database

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,22 @@ import { User } from './entity/User.entity';
 
 createConnection()
   .then(async (connection) => {
-    const user = new User('Timber', 'Saw', 25);
-    await connection.manager.save(user);
-
+    const user: User = new User('Timber', 'Saw', 25);
     const photo: Photo = new Photo('Me and Bears', 'I am near polar bears', 'photo-with-bears.jpg', 1, true);
-    await connection.manager.save(photo).then((photo: Photo) => console.log(`Photo has been saved. Photo id is ${photo.id}`));
-    const savedPhotos: Array<Photo> = await connection.manager.find(Photo);
+    const userRepository = connection.getRepository(User);
+    const photoRepository = connection.getRepository(Photo);
+
+    await userRepository.save(user);
+    await photoRepository.save(photo);
+
+    const savedPhotos: Array<Photo> = await photoRepository.find();
+    const photoById: Photo = await photoRepository.findOne(3);
+    photoById.name = 'Me, my friends and polar bears';
+    await photoRepository.save(photoById);
+    await photoRepository.remove(await photoRepository.findOne(2));
+    
     console.log('All photos from the db: ', savedPhotos);
+    console.log(`Photo from the db with id: ${photoById.id}`);
     
   })
   .catch((error) => console.log(error));
