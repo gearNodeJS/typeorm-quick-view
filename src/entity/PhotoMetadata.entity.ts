f95ff4152018bb1ee2e0065a224b38ba474ced67
Valery Semenenko <gearmobile@gmail.com>
2021-01-04 20:02:38 +0300

    one-to-one relation

---
+++ src/entity/PhotoMetadata.entity.ts
@@ -0,0 +1,21 @@
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+
+export class PhotoMetadata {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('int')
+  height: number;
+
+  @Column('int')
+  width: number;
+
+  @Column()
+  orientation: string;
+
+  @Column()
+  compressed: boolean;
+
+  @Column()
+  comment: string;
+}
